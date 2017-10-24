6bdb9c362141b1b4ea3a23c4b654ed9cb351bb5d
Lovell Fuller <github@lovell.info>
2017-10-24 17:35:19 +0100

    Use nyc for test coverage
    Use existing coverage as threshold values
    
    Bump mocha back to latest that supports Node 0.12

--- gruntfile.js
+++ gruntfile.js
@@ -28,30 +28,6 @@ module.exports = function(grunt) {
           reporter: 'spec'
         },
         src: ['test/**/*.js']
-      },
-      testCoverage: {
-        options: {
-          reporter: 'spec',
-          require: 'test/coverage/blanket'
-        },
-        src: ['test/**/*.js']
-      },
-      coverage: {
-        options: {
-          reporter: 'html-cov',
-          // use the quiet flag to suppress the mocha console output
-          quiet: true,
-          // specify a destination file to capture the mocha
-          // output (the quiet option does not suppress this)
-          captureFile: 'coverage.html'
-        },
-        src: ['test/**/*.js']
-      },
-      'travis-cov': {
-        options: {
-          reporter: 'travis-cov'
-        },
-        src: ['test/**/*.js']
       }
     }
   });
@@ -87,7 +63,4 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['jshint', 'dynamo:start', 'mochaTest']);
 
   grunt.registerTask('test', ['jshint', 'dynamo:start', 'mochaTest:test']);
-
-  grunt.registerTask('coverage', ['jshint', 'dynamo:start', 'mochaTest:testCoverage', 'mochaTest:coverage', 'mochaTest:travis-cov']);
-
 };
