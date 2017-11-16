ddb7b46058dd6638ada990996d1e33b434aeab7f
Dilip Kola <dilip@tensult.com>
2017-11-16 11:56:29 +0530

    Change PutOptions as optional fields

--- dynamoose.d.ts
+++ dynamoose.d.ts
@@ -127,19 +127,19 @@ declare module "dynamoose" {
     /**
      * Overwrite existing item. Defaults to true.
      */
-    overwrite: boolean;
+    overwrite?: boolean;
     /**
      * An expression for a conditional update. See the AWS documentation for more information about condition expressions.
      */
-    condition: string;
+    condition?: string;
     /**
     * A map of name substitutions for the condition expression.
     */
-    conditionNames: any;
+    conditionNames?: any;
     /**
     * A map of values for the condition expression. Note that in order for automatic object conversion to work, the keys in this object must match schema attribute names.
     */
-    conditionValues: any;
+    conditionValues?: any;
   }
   type SaveOptions = PutOptions;
 
