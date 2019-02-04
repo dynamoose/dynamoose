52333820163523136733efdfeff90c5d7ad7b469
Charlie Fish <fishcharlie@me.com>
2019-02-03 18:46:54 -0700

    Fixing warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
+  'env': {
+    'es6': true,
+    'node': true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": 2018
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 2018
   },
-  "rules": {
-    "indent": [
-      "error",
+  'rules': {
+    'indent': [
+      'error',
       2
     ],
-    "linebreak-style": [
-      "error",
-      "unix"
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "quotes": [
-      "error",
-      "single"
+    'quotes': [
+      'error',
+      'single'
     ],
-    "semi": [
-      "error",
-      "always"
-    ]
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-console': 'off'
   }
 };
