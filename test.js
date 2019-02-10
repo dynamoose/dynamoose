d4044aa5b40ce90b00e5d8f3ebb0358700b9f0ee
Charlie Fish <fishcharlie@me.com>
2019-02-09 22:59:42 -0700

    test(test): general test improvements

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 
-console.log(AWS.DynamoDB.Converter.output({"NS": [1,2,3]}));
+console.log(AWS.DynamoDB.Converter.output({'NS': [1,2,3]}));
 console.log(new Set([1,2,3]));
-console.log(AWS.DynamoDB.Converter.input([1,2,3]))
+console.log(AWS.DynamoDB.Converter.input([1,2,3]));
