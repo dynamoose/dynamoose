8ddc7ee00b1a8c90c03ab27fed05901d84ca638d
cweeks <harris_weeks@intuit.com>
2019-02-12 12:17:39 -0800

    fix(bug): starting work on issue 323

---
+++ issue_reproduction/323/index.js
@@ -0,0 +1,27 @@
+const dynamoose = require('../../lib');
+const dynalite = require('dynalite');
+
+const startUpAndReturnDynamo = async () => {
+  const dynaliteServer = dynalite();
+  await dynaliteServer.listen(8000);
+  return dynaliteServer;
+};
+
+const createDynamooseInstance = () => {
+  dynamoose.AWS.config.update({
+    accessKeyId: 'AKID',
+    secretAccessKey: 'SECRET',
+    region: 'us-east-1'
+  });
+  dynamoose.local(); // This defaults to "http://localhost:8000"
+  return dynamoose;
+};
+
+const initialize = async () => {
+  await startUpAndReturnDynamo();
+  return createDynamooseInstance();
+};
+
+module.exports = {
+  initialize
+};
