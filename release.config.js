b42b17af8a0ce334d65b518b205294b25ceb0530
hweeks <root@hweeks.com>
2019-02-12 20:31:50 -0800

    ci(revert): pull release changes

--- release.config.js
+++ release.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    ['@semantic-release/changelog', {
-      'changelogFile': 'CHANGELOG.md',
-    }],
-    '@semantic-release/git'
-  ]
+  plugins: ['@semantic-release/npm', '@semantic-release/changelog', '@semantic-release/git']
 };
