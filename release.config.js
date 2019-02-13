aea8a80a86f32a7484f0ceb44a6ae464a6816d94
hweeks <root@hweeks.com>
2019-02-12 19:40:00 -0800

    ci(changelog): fix changelog output

--- release.config.js
+++ release.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: ['@semantic-release/npm', '@semantic-release/changelog', '@semantic-release/git']
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    ['@semantic-release/changelog', {
+      'changelogFile': 'CHANGELOG.md',
+    }],
+    '@semantic-release/git'
+  ]
 };
