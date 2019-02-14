8034c8f52cf6b1fcb969534be0d57d8220928b34
Charlie Fish <fishcharlie@me.com>
2019-02-14 07:21:47 -0800

    Update docs/javascripts/init.js
    Co-Authored-By: hweeks <h.weeks@gmail.com>

--- docs/javascripts/init.js
+++ docs/javascripts/init.js
@@ -8,5 +8,5 @@ jscode.style.fontFamily = 'Roboto Mono';
 var bashcode = document.getElementById('bash-code')
 bashcode.style.backgroundColor = '#f9fbfc'
 bashcode.style.fontSize = '14px'
-bashcode.style.lineHeight = '30px'
+bashcode.style.lineHeight = '30px';
 bashcode.style.fontFamily = 'Roboto Mono'
