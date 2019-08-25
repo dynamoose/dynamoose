878c9aca116bb330fc7b89ebb0bc94fc0c69979e
Charlie Fish <fishcharlie@me.com>
2019-08-24 20:26:51 -0600

    Removing old commitizen and husky code

--- release.config.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = {
-  'plugins': ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', ['@semantic-release/changelog', {'changelogTitle': '# Dynamoose Changelog'}], '@semantic-release/npm', '@semantic-release/git']
-};
