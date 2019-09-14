43c67f639838d0494a9427e55129fb1ac8dd0407
Charlie Fish <fishcharlie@me.com>
2019-09-14 10:50:05 -0600

    Fixing issue with new keyword for creating a model

--- manual_test/index.js
+++ manual_test/index.js
@@ -1,6 +1,10 @@
 const dynamoose = require("../lib");
+const dynamooseOld = require("dynamoose");
 
-const Cat = dynamoose.model("Cat", { "name": String });
+// const Cat = dynamoose.model("Cat", { "name": String });
 
 // const kitty = new Cat({ name: 'Zildjian' });
 // kitty.save().then(() => console.log('meow'));
+
+const modelA = dynamooseOld.model("Cat", {"name": String});
+const modelB = new dynamooseOld.model("CatB", {"name": String});
