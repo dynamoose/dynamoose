00b7dd0d586764ad1a30bf0d6445765d0a7fc79b
Charlie Fish <fishcharlie@me.com>
2019-12-14 11:07:47 -0700

    Verifying with manual tests

--- manual_test/index.js
+++ manual_test/index.js
@@ -1,28 +1,39 @@
 const dynamoose = require("../lib");
 const dynamooseOld = require("dynamoose");
 
-// const Cat = dynamoose.model("Cat", { "name": String });
-
-// console.log(Object.getPrototypeOf(Cat));
-// Cat();
-// console.log(Cat instanceof dynamoose.model);
-// const kitty = new Cat({ name: 'Zildjian' });
-// kitty.save().then(() => console.log('meow'));
-
-const sdk = dynamoose.aws.sdk; // require("aws-sdk");
-sdk.config.update({
+dynamooseOld.AWS.config.update({
 	"accessKeyId": "AKID",
 	"secretAccessKey": "SECRET",
 	"region": "us-east-1"
 });
-const ddb = new dynamoose.aws.sdk.DynamoDB({"endpoint": "http://localhost:8000"});
-dynamoose.aws.ddb.set(ddb);
+dynamooseOld.local();
 
-const Cat = dynamoose.model("Cat7", {"id": Number, "name": String});
+const Cat = dynamooseOld.model("Cat101", { "id": Number, "name": String });
 
-async function main() {
-	await (new Cat({"id": 5, "name": "Hello World"}).save());
+// console.log(Object.getPrototypeOf(Cat));
+// Cat();
+// console.log(Cat instanceof dynamoose.model);
+const kitty = new Cat({ "id": 1, name: 'Zildjian' });
+kitty.save().then((res) => console.log(typeof res)).catch((err) => console.error(err));
 
-	console.log(JSON.stringify(await ddb.scan({"TableName": "Cat7"}).promise(), null, 4));
-}
-main();
+
+
+
+
+// const sdk = dynamoose.aws.sdk; // require("aws-sdk");
+// sdk.config.update({
+// 	"accessKeyId": "AKID",
+// 	"secretAccessKey": "SECRET",
+// 	"region": "us-east-1"
+// });
+// const ddb = new dynamoose.aws.sdk.DynamoDB({"endpoint": "http://localhost:8000"});
+// dynamoose.aws.ddb.set(ddb);
+//
+// const Cat = dynamoose.model("Cat7", {"id": Number, "name": String});
+//
+// async function main() {
+// 	await (new Cat({"id": 5, "name": "Hello World"}).save());
+//
+// 	console.log(JSON.stringify(await ddb.scan({"TableName": "Cat7"}).promise(), null, 4));
+// }
+// main();
