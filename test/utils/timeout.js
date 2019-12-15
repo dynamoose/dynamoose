8b3f3cba56d5619c40b516b5acc2c1f8def39ca1
Charlie Fish <fishcharlie@me.com>
2019-12-15 12:32:09 -0700

    Fixing random timeout test bug

--- test/utils/timeout.js
+++ test/utils/timeout.js
@@ -15,7 +15,7 @@ describe("Timeout", () => {
 		const ms = 50;
 		const timeA = Date.now();
 		await utils.timeout(ms);
-		expect(Date.now() - timeA).to.be.at.least(ms);
+		expect(Date.now() - timeA).to.be.at.least(ms - 1); // Doing -1 here due to https://github.com/nodejs/node/issues/26578
 	});
 
 	it("Should reject if invalid number passed in", async () => {
