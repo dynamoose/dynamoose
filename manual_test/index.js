58733dd94ceb2407f9bee389c89929f9c7f8410f
Charlie Fish <fishcharlie@me.com>
2019-12-26 22:05:30 -0700

    Adding basic support for Scan

--- manual_test/index.js
+++ manual_test/index.js
@@ -1,5 +1,6 @@
 const dynamoose = require("../lib");
 const dynamooseOld = require("dynamoose");
+const expect = require("chai").expect;
 
 // dynamooseOld.AWS.config.update({
 // 	"accessKeyId": "AKID",
@@ -32,15 +33,32 @@ const ddb = new dynamoose.aws.sdk.DynamoDB({"endpoint": "http://localhost:8000"}
 dynamoose.aws.ddb.set(ddb);
 dynamooseOld.setDDB(ddb);
 
-const Cat = dynamooseOld.model("Cat200", {"id": Number, "name": Number}, {"create": false});
-const CatB = new dynamoose.model("Cat200", {"id": Number, "name": Number}, {"create": false});
+const Cat = dynamooseOld.model("Cat200", {"id": Number, "name": String, "breed": String}, {"create": false});
+const CatB = new dynamoose.model("Cat200", {"id": Number, "name": String, "breed": String}, {"create": false});
 
 async function main() {
-	const cat = new Cat({"id": 10, "other": "Test", "name": "test"});
-	console.log(cat);
-	const other = await cat.save();
-	console.log(other);
-	console.log(await Cat.get(10));
+	// const cat = new Cat({"id": 10, "other": "Test", "name": "test"});
+	// console.log(cat);
+	// const other = await cat.save();
+	// console.log(other);
+	// console.log(await Cat.get(10));
+
+	// for (let i = 0; i < 1000; i++) {
+	// 	const cat = new CatB({"id": i, "name": new Array(50000).fill("a").join("")});
+	// 	await cat.save();
+	// }
+
+	// console.log((await CatB.scan({breed: {contains: 'Terrier'}}).exec()));
+	// console.log(await Cat.scan({breed: {contains: 'Terrier'}}).exec());
+
+	// console.log(await CatB.scan().exec());
+	// console.log(await Cat.scan().exec());
+
+	// console.log((await CatB.scan().parallel(2).exec()));
+	// console.log((await Cat.scan().parallel(2).exec()));
+	console.log((await CatB.scan().attributes(["id"]).exec()));
+	console.log((await Cat.scan().attributes(["id"]).exec()));
+
 	// console.log(await Cat.get(500));
 	// console.log(await CatB.get(500));
 	// console.log(await dynamoose.aws.ddb().getItem({
