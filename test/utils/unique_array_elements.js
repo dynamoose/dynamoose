cb55c4e681a29d76fd8f0bf1b4ea70c2288792d1
Charlie Fish <fishcharlie@me.com>
2020-03-08 15:00:10 -0600

    Adding tests for unique_array_elements utility function

---
+++ test/utils/unique_array_elements.js
@@ -0,0 +1,21 @@
+const chai = require("chai");
+const {expect} = chai;
+const utils = require("../../lib/utils");
+
+describe("unique_array_elements", () => {
+	it("Should be a function", () => {
+		expect(utils.unique_array_elements).to.be.a("function");
+	});
+
+	const tests = [
+		{"input": [], "output": []},
+		{"input": [1, 1], "output": [1]},
+		{"input": [1, 2, 3, 1], "output": [1, 2, 3]},
+		{"input": ["test", "TEST", "tesT", "test"], "output": ["test", "TEST", "tesT"]}
+	];
+	tests.forEach((test) => {
+		it(`Should return ${test.output} for ${test.input}`, () => {
+			expect(utils.unique_array_elements(test.input)).to.eql(test.output);
+		});
+	});
+});
