30eaa23fc1fbfbee921b4d2f7ef2e2eedcf05b50
Charlie Fish <fishcharlie@me.com>
2020-03-28 13:08:50 -0600

    Adding utils.object.entries function

---
+++ test/utils/object/entries.js
@@ -0,0 +1,37 @@
+const {expect} = require("chai");
+const utils = require("../../../lib/utils");
+
+describe("utils.object.entries", () => {
+	it("Should be a function", () => {
+		expect(utils.object.entries).to.be.a("function");
+	});
+
+	const tests = [
+		{
+			"input": {"hello": "world"},
+			"output": [["hello", "world"]]
+		},
+		{
+			"input": {"hello": "world", "test": "data"},
+			"output": [["hello", "world"], ["test", "data"]]
+		},
+		{
+			"input": {"name": "Bob", "address": {"country": "world", "zip": 12345}},
+			"output": [["name", "Bob"], ["address", {"country": "world", "zip": 12345}], ["address.country", "world"], ["address.zip", 12345]]
+		},
+		{
+			"input": {"name": "Bob", "friends": ["Bob", "Tim"]},
+			"output": [["name", "Bob"], ["friends", ["Bob", "Tim"]], ["friends.0", "Bob"], ["friends.1", "Tim"]]
+		},
+		{
+			"input": {"id": 1, "friends": [{"name": "Bob", "id": 1}, {"name": "Tim"}]},
+			"output": [["id", 1], ["friends", [{"name": "Bob", "id": 1}, {"name": "Tim"}]], ["friends.0", {"name": "Bob", "id": 1}], ["friends.0.name", "Bob"], ["friends.0.id", 1], ["friends.1", {"name": "Tim"}], ["friends.1.name", "Tim"]]
+		}
+	];
+
+	tests.forEach((test) => {
+		it(`Should return ${test.output} for ${test.input}`, () => {
+			expect(utils.object.entries(test.input)).to.eql(test.output);
+		});
+	});
+});
