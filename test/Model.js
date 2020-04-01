62fcffdbd365d67820941666401ceb69c35a206c
Charlie Fish <fishcharlie@me.com>
2020-03-31 18:35:17 -0600

    Adding tests for custom methods on Documents

--- test/Model.js
+++ test/Model.js
@@ -2621,12 +2621,14 @@ describe("Model", () => {
 	});
 
 	describe("Model.methods", () => {
-		let User;
+		let User, user;
 		beforeEach(() => {
 			User = new dynamoose.Model("User", {"id": Number, "name": String});
+			user = new User();
 		});
 		afterEach(() => {
 			User = null;
+			user = null;
 		});
 
 		it("Should be an object", () => {
@@ -2754,6 +2756,135 @@ describe("Model", () => {
 				expect(User.myMethod).to.be.a("function");
 				User.methods.delete("myMethod");
 				expect(User.myMethod).to.eql(undefined);
+			});
+		});
+
+		describe("Model.methods.document", () => {
+			describe("Model.methods.document.set", () => {
+				it("Should be a function", () => {
+					expect(User.methods.document.set).to.be.a("function");
+				});
+
+				it("Should not throw an error when being called", () => {
+					expect(() => User.methods.document.set("random", () => {})).to.not.throw();
+				});
+
+				it("Should set correct method on model", () => {
+					User.methods.document.set("random", () => {});
+					expect(user.random).to.be.a("function");
+				});
+
+				it("Should not overwrite internal methods", () => {
+					const originalMethod = user.save;
+					const newMethod = () => {};
+					User.methods.document.set("save", newMethod);
+					expect(user.save).to.eql(originalMethod);
+					expect(user.save).to.not.eql(newMethod);
+				});
+
+				it("Should overwrite methods if Internal.internalProperties exists but type doesn't", () => {
+					const originalMethod = user.save;
+					const newMethod = () => {};
+					User.prototype.random = originalMethod;
+					User.prototype.random[Internal.internalProperties] = {};
+					User.methods.document.set("random", newMethod);
+					expect(user.random).to.eql(originalMethod);
+					expect(user.random).to.not.eql(newMethod);
+				});
+
+				const methodTypes = [
+					{"name": "Callback", "func": (func) => {
+						return function (...args) {
+							const cb = args[args.length - 1];
+							func.bind(this)(...args).then((result) => cb(null, result)).catch((err) => cb(err));
+						};
+					}},
+					{"name": "Promise", "func": (func) => func}
+				];
+				methodTypes.forEach((methodType) => {
+					describe(`Method Type - ${methodType.name}`, () => {
+						const callerTypes = [
+							{"name": "Callback", "func": util.promisify},
+							{"name": "Promise", "func": (func) => func}
+						];
+						callerTypes.forEach((callerType) => {
+							describe(`Caller Type - ${callerType.name}`, () => {
+								let methodTypeCallDetails = [], methodTypeCallResult = {};
+								beforeEach(() => {
+									User.methods.document.set("action", methodType.func(async function (...args) {
+										methodTypeCallDetails.push({"arguments": [...args], "this": this});
+										if (methodTypeCallResult.error) {
+											throw methodTypeCallResult.error;
+										} else if (methodTypeCallResult.result) {
+											return methodTypeCallResult.result;
+										}
+									}));
+								});
+								afterEach(() => {
+									methodTypeCallDetails = [];
+									methodTypeCallResult = {};
+								});
+
+								it("Should call custom method with correct arguments", async () => {
+									methodTypeCallResult.result = "Success";
+									await callerType.func(user.action)();
+									expect(methodTypeCallDetails.length).to.eql(1);
+									expect(methodTypeCallDetails[0].arguments.length).to.eql(1);
+									expect(methodTypeCallDetails[0].arguments[0]).to.be.a("function");
+								});
+
+								it("Should call custom method with correct `this`", async () => {
+									methodTypeCallResult.result = "Success";
+									// Not sure why we have to bind `user` here, when we don't have to bind anything for Model.methods.set
+									// Through manual testing tho, you do not need to bind anything and in production use this works as described in the documentation
+									// Would be nice to figure out why this is only necessary for our test suite and fix it
+									await callerType.func(user.action).bind(user)();
+									expect(methodTypeCallDetails[0].this).to.eql(user);
+								});
+
+								it("Should return correct response that custom method returns", async () => {
+									methodTypeCallResult.result = "Success";
+									const result = await callerType.func(user.action)();
+									expect(result).to.eql("Success");
+								});
+
+								it("Should throw error if custom method throws error", async () => {
+									methodTypeCallResult.error = "ERROR";
+									let result, error;
+									try {
+										result = await callerType.func(user.action)();
+									} catch (e) {
+										error = e;
+									}
+									expect(result).to.not.exist;
+									expect(error).to.eql("ERROR");
+								});
+							});
+						});
+					});
+				});
+			});
+
+			describe("Model.methods.document.delete", () => {
+				it("Should be a function", () => {
+					expect(User.methods.document.delete).to.be.a("function");
+				});
+
+				it("Should not delete internal methods", () => {
+					User.methods.document.delete("save");
+					expect(user.save).to.be.a("function");
+				});
+
+				it("Should not throw error for deleting unknown method", () => {
+					expect(() => User.methods.document.delete("randomHere123")).to.not.throw();
+				});
+
+				it("Should delete custom method", () => {
+					User.methods.document.set("myMethod", () => {});
+					expect(user.myMethod).to.be.a("function");
+					User.methods.document.delete("myMethod");
+					expect(user.myMethod).to.eql(undefined);
+				});
 			});
 		});
 	});
