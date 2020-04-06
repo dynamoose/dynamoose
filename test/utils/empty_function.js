e8f52ab89d21b2d9b8d8a1905d43656321490ef2
Charlie Fish <fishcharlie@me.com>
2020-04-05 20:21:44 -0600

    Refactoring DocumentRetriever code to use Condition functions

---
+++ test/utils/empty_function.js
@@ -0,0 +1,12 @@
+const {expect} = require("chai");
+const utils = require("../../lib/utils");
+
+describe("utils.empty_function", () => {
+	it("Should be a function", () => {
+		expect(utils.empty_function).to.be.a("function");
+	});
+
+	it("Should equal an empty function", () => {
+		expect(utils.empty_function.toString()).to.eql("()=>{}");
+	});
+});
