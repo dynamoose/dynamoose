c60e21398144ddd78a4eebb4a89ab428ef78a4af
Charlie Fish <fishcharlie@me.com>
2020-04-07 22:48:57 -0600

    Modifying set_immediate_promise to TypeScript

--- lib/utils/set_immediate_promise.ts
+++ lib/utils/set_immediate_promise.ts
@@ -1,2 +1,2 @@
 // This function is used to turn `setImmediate` into a promise. This is espescially useful if you want to wait for pending promises to fire and complete before running the asserts on a test.
-module.exports = () => new Promise((resolve) => setImmediate(resolve));
+export = (): Promise<undefined> => new Promise((resolve) => setImmediate(resolve));
