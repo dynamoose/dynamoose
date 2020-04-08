73c8233c75968bc355dcba8b0297ec3a14914ccb
Charlie Fish <fishcharlie@me.com>
2020-04-07 23:42:12 -0600

    Converting object.index utility to TypeScript

--- lib/utils/object/index.ts
+++ lib/utils/object/index.ts
@@ -1,8 +1,15 @@
-module.exports = {
-	"get": require("./get"),
-	"set": require("./set"),
-	"delete": require("./delete"),
-	"pick": require("./pick"),
-	"keys": require("./keys"),
-	"entries": require("./entries")
+import get from "./get";
+import set from "./set";
+import deleteFunc from "./delete";
+import pick from "./pick";
+import keys from "./keys";
+import entries from "./entries";
+
+export = {
+	get,
+	set,
+	"delete": deleteFunc,
+	pick,
+	keys,
+	entries
 };
