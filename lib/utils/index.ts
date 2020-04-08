ac3b01e5d28de464efbfd92fad99ba820cb9f98b
Charlie Fish <fishcharlie@me.com>
2020-04-07 23:20:32 -0600

    Modifying utils.index to TypeScript

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,12 +1,23 @@
-module.exports = {
-	"combine_objects": require("./combine_objects"),
-	"merge_objects": require("./merge_objects"),
-	"timeout": require("./timeout"),
-	"capitalize_first_letter": require("./capitalize_first_letter"),
-	"set_immediate_promise": require("./set_immediate_promise"),
-	"unique_array_elements": require("./unique_array_elements"),
-	"array_flatten": require("./array_flatten"),
-	"empty_function": require("./empty_function"),
-	"object": require("./object"),
-	"dynamoose": require("./dynamoose")
+import combine_objects from "./combine_objects";
+import merge_objects from "./merge_objects";
+import timeout from "./timeout";
+import capitalize_first_letter from "./capitalize_first_letter";
+import set_immediate_promise from "./set_immediate_promise";
+import unique_array_elements from "./unique_array_elements";
+import array_flatten from "./array_flatten";
+import empty_function from "./empty_function";
+import object from "./object";
+import dynamoose from "./dynamoose";
+
+export = {
+	combine_objects,
+	merge_objects,
+	timeout,
+	capitalize_first_letter,
+	set_immediate_promise,
+	unique_array_elements,
+	array_flatten,
+	empty_function,
+	object,
+	dynamoose
 };
