b41bae2c9e86f75f23df02cd2190b17b1d942654
Charlie Fish <fishcharlie@me.com>
2020-04-07 22:48:08 -0600

    Modifying timeout to TypeScript

--- lib/utils/timeout.ts
+++ lib/utils/timeout.ts
@@ -1,8 +1,8 @@
-module.exports = (ms) => {
-	ms = parseInt(ms);
+export = (time: string | number): Promise<undefined> => {
+	const ms: number = typeof time === "string" ? parseInt(time) : time;
 	return new Promise((resolve, reject) => {
 		if (isNaN(ms)) {
-			reject(`Invalid miliseconds passed in: ${ms}`);
+			reject(`Invalid miliseconds passed in: ${time}`);
 		}
 		setTimeout(() => resolve(), ms);
 	});
