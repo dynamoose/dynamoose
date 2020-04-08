4c6b799a30a867666e590504543b9544b475e80f
Charlie Fish <fishcharlie@me.com>
2020-04-07 22:42:22 -0600

    Modifying lib/index.ts to work with TypeScript

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,14 @@
-module.exports = {
-	"Model": require("./Model"),
-	"Schema": require("./Schema"),
-	"Condition": require("./Condition"),
-	"transaction": require("./Transaction"),
-	"aws": require("./aws"),
+import Model from "./Model";
+import Schema from "./Schema";
+import Condition from "./Condition";
+import transaction from "./transaction";
+import aws from "./aws";
+
+export = {
+	Model,
+	Schema,
+	Condition,
+	transaction,
+	aws,
 	"undefined": Symbol("dynamoose.undefined")
 };
