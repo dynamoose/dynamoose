132ecaefafaac566060172139946c2a831c006b6
Charlie Fish <fishcharlie@me.com>
2020-04-08 19:52:22 -0600

    Modifying usage of Error to not use `new` keyword

--- lib/ModelStore.ts
+++ lib/ModelStore.ts
@@ -10,7 +10,7 @@ const returnObject = (input: Model | string): Model | never => {
 	} else if (typeof input === "string") {
 		return models[input];
 	} else {
-		throw new CustomError.InvalidParameter("You must pass in a Model or table name as a string.");
+		throw CustomError.InvalidParameter("You must pass in a Model or table name as a string.");
 	}
 };
 returnObject.clear = (): void => {
