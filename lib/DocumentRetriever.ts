4b2d4e9977558e03478a15ccaa3cc0306e0fee0a
Charlie Fish <fishcharlie@me.com>
2020-04-09 18:31:04 -0600

    Modifying DocumentRetirever to TypeScript

--- lib/DocumentRetriever.ts
+++ lib/DocumentRetriever.ts
@@ -1,50 +1,70 @@
-const aws = require("./aws");
-const Error = require("./Error");
-const utils = require("./utils");
-const Condition = require("./Condition");
+import aws from "./aws";
+import CustomError from "./Error";
+import utils from "./utils";
+import Condition from "./Condition";
+
+
+type ConditionInitalizer = Condition | {[key: string]: any} | string;
+
 
 // DocumentRetriever is used for both Scan and Query since a lot of the code is shared between the two
 
-const documentRetrieverTypes = [
+const documentRetrieverTypes: {type: "scan" | "query"; pastTense: "scanned" | "queried"}[] = [
 	{"type": "scan", "pastTense": "scanned"},
 	{"type": "query", "pastTense": "queried"}
 ];
-
-function main(documentRetrieverTypeString) {
+function main(documentRetrieverTypeString: "scan" | "query") {
 	const documentRetrieverType = documentRetrieverTypes.find((a) => a.type === documentRetrieverTypeString);
 
 	if (!documentRetrieverType) {
-		throw Error.InvalidType(`The type: ${documentRetrieverTypeString} for setting up a document retriever is invalid.`);
+		throw CustomError.InvalidType(`The type: ${documentRetrieverTypeString} for setting up a document retriever is invalid.`);
 	}
 
 	function Carrier(model) {
-		let C = class {
-			constructor(object) {
-				this.settings = {};
-				this.settings.limit = null;
+		class C {
+			settings: {
+				condition: Condition;
+				limit?: number;
+				all?: {delay: number; max: number};
+				startAt?: any;
+				attributes?: string[];
+				index?: string;
+				consistent?: boolean;
+				count?: boolean;
+				parallel?: number;
+			};
+			getRequest: (this: C) => Promise<any>;
+			exec: (this: C, callback) => any;
+			all: (this: C, delay?: number, max?: number) => C;
 
+			constructor(object?: ConditionInitalizer) {
+				let condition: Condition;
 				try {
-					this.settings.condition = new Condition(object);
+					condition = new Condition(object);
 				} catch (e) {
 					e.message = `${e.message.replace(" is invalid.", "")} is invalid for the ${documentRetrieverType.type} operation.`;
 					throw e;
 				}
 
+				this.settings = {
+					"condition": condition
+				};
+
 				return this;
 			}
-		};
+		}
 		Object.entries(Condition.prototype).forEach((prototype) => {
 			const [key, func] = prototype;
 			if (key !== "requestObject") {
-				C.prototype[key] = function(...args) {
+				C.prototype[key] = function(this: C, ...args): C {
 					func.bind(this.settings.condition)(...args);
 					return this;
 				};
 			}
 		});
-		C.prototype[`get${utils.capitalize_first_letter(documentRetrieverType.type)}Request`] = async function() {
-			const object = {
-				...this.settings.condition.requestObject({"defaultPrefix": "", "conditionString": "FilterExpression"}),
+		C.prototype.getRequest = async function(this: C): Promise<any> {
+			const object: any = {
+				...this.settings.condition.requestObject({"conditionString": "FilterExpression"}),
 				"TableName": model.name
 			};
 
@@ -52,7 +72,7 @@ function main(documentRetrieverTypeString) {
 				object.Limit = this.settings.limit;
 			}
 			if (this.settings.startAt) {
-				object.ExclusiveStartKey = model.Document.isDynamoObject(this.settings.startAt) ? this.settings.startAt : model.Document.toDynamo(this.settings.startAt);
+				object.ExclusiveStartKey = model.Document.isDynamoObject(this.settings.startAt) ? this.settings.startAt : model.Document.objectToDynamo(this.settings.startAt);
 			}
 			if (this.settings.attributes) {
 				object.AttributesToGet = this.settings.attributes;
@@ -74,11 +94,11 @@ function main(documentRetrieverTypeString) {
 					return (comparisonChart[hash] || {}).type === "EQ" && (!range || !comparisonChart[range] || comparisonChart[range].type === "EQ");
 				});
 				if (!index) {
-					throw Error.InvalidParameter("Index can't be found for query.");
+					throw CustomError.InvalidParameter("Index can't be found for query.");
 				}
 				object.IndexName = index.IndexName;
 			}
-			function moveParameterNames(val, prefix) {
+			function moveParameterNames(val, prefix): void {
 				const entry = Object.entries(object.ExpressionAttributeNames).find((entry) => entry[1] === val);
 				if (!entry) {
 					return;
@@ -124,9 +144,9 @@ function main(documentRetrieverTypeString) {
 
 			return object;
 		};
-		C.prototype.exec = function(callback) {
+		C.prototype.exec = function(this: C, callback): Promise<any> | void {
 			let timesRequested = 0;
-			const prepareForReturn = async (result) => {
+			const prepareForReturn = async (result): Promise<any> => {
 				if (Array.isArray(result)) {
 					result = utils.merge_objects(...result);
 				}
@@ -136,14 +156,14 @@ function main(documentRetrieverTypeString) {
 						[`${documentRetrieverType.pastTense}Count`]: result[`${utils.capitalize_first_letter(documentRetrieverType.pastTense)}Count`]
 					};
 				}
-				const array = (await Promise.all(result.Items.map(async (item) => await ((new model.Document(item, {"fromDynamo": true})).conformToSchema({"customTypesDynamo": true, "checkExpiredItem": true, "saveUnknown": true, "modifiers": ["get"], "type": "fromDynamo"}))))).filter((a) => Boolean(a));
+				const array: any = (await Promise.all(result.Items.map(async (item) => await ((new model.Document(item, {"fromDynamo": true})).conformToSchema({"customTypesDynamo": true, "checkExpiredItem": true, "saveUnknown": true, "modifiers": ["get"], "type": "fromDynamo"}))))).filter((a) => Boolean(a));
 				array.lastKey = result.LastEvaluatedKey ? (Array.isArray(result.LastEvaluatedKey) ? result.LastEvaluatedKey.map((key) => model.Document.fromDynamo(key)) : model.Document.fromDynamo(result.LastEvaluatedKey)) : undefined;
 				array.count = result.Count;
 				array[`${documentRetrieverType.pastTense}Count`] = result[`${utils.capitalize_first_letter(documentRetrieverType.pastTense)}Count`];
 				array[`times${utils.capitalize_first_letter(documentRetrieverType.pastTense)}`] = timesRequested;
 				return array;
 			};
-			const promise = model.pendingTaskPromise().then(() => this[`get${utils.capitalize_first_letter(documentRetrieverType.type)}Request`]()).then((request) => {
+			const promise = model.pendingTaskPromise().then(() => this.getRequest()).then((request) => {
 				const ddb = aws.ddb();
 
 				const allRequest = (extraParameters = {}) => {
@@ -190,14 +210,20 @@ function main(documentRetrieverTypeString) {
 			if (callback) {
 				promise.then((result) => prepareForReturn(result)).then((result) => callback(null, result)).catch((error) => callback(error));
 			} else {
-				return (async () => {
+				return (async (): Promise<any> => {
 					const result = await promise;
 					const finalResult = await prepareForReturn(result);
 					return finalResult;
 				})();
 			}
 		};
-		const settings = [
+		interface SettingDefinition {
+			name: string;
+			only?: ("scan" | "query")[];
+			boolean?: boolean;
+			settingsName?: string;
+		}
+		const settings: (SettingDefinition | string)[] = [
 			"limit",
 			"startAt",
 			"attributes",
@@ -207,15 +233,15 @@ function main(documentRetrieverTypeString) {
 			{"name": "using", "settingsName": "index"}
 		];
 		settings.forEach((item) => {
-			if (!item.only || item.only.includes(documentRetrieverType.type)) {
-				C.prototype[item.name || item] = function(value) {
-					const key = item.settingsName || item.name || item;
-					this.settings[key] = item.boolean ? !this.settings[key] : value;
+			if (!(item as SettingDefinition).only || (item as SettingDefinition).only.includes(documentRetrieverType.type)) {
+				C.prototype[(item as SettingDefinition).name || (item as string)] = function(value): C {
+					const key: string = (item as SettingDefinition).settingsName || (item as SettingDefinition).name || (item as string);
+					this.settings[key] = (item as SettingDefinition).boolean ? !this.settings[key] : value;
 					return this;
 				};
 			}
 		});
-		C.prototype.all = function(delay = 0, max = 0) {
+		C.prototype.all = function(this: C, delay = 0, max = 0): C {
 			this.settings.all = {delay, max};
 			return this;
 		};
@@ -227,4 +253,4 @@ function main(documentRetrieverTypeString) {
 	return Carrier;
 }
 
-module.exports = main;
+export = main;
