3ec3086eeaaafc2bc190fb6e2c63bf8619ebba35
Charlie Fish <fishcharlie@me.com>
2020-04-09 21:50:07 -0600

    Adding query tests for no index or non equals range key comparison

--- test/Query.js
+++ test/Query.js
@@ -143,6 +143,153 @@ describe("Query", () => {
 					});
 				});
 
+				it("Should send correct request on query.exec if querying main key", async () => {
+					queryPromiseResolver = () => ({"Items": []});
+					Model = new dynamoose.Model("Cat", {"id": String, "name": String, "age": Number});
+					await callType.func(Model.query("id").eq("HelloWorld").exec).bind(Model.query("id").eq("HelloWorld"))();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"ExpressionAttributeNames": {
+							"#qha": "id"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "HelloWorld"}
+						},
+						"KeyConditionExpression": "#qha = :qhv"
+					});
+				});
+
+				it("Should send correct request on query.exec if querying main key with range key", async () => {
+					queryPromiseResolver = () => ({"Items": []});
+					Model = new dynamoose.Model("Cat", {"id": String, "name": {"type": String, "rangeKey": true}, "age": Number});
+					await callType.func(Model.query("id").eq("HelloWorld").where("name").eq("Charlie").exec).bind(Model.query("id").eq("HelloWorld").where("name").eq("Charlie"))();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"ExpressionAttributeNames": {
+							"#qha": "id",
+							"#qra": "name"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "HelloWorld"},
+							":qrv": {"S": "Charlie"}
+						},
+						"KeyConditionExpression": "#qha = :qhv AND #qra = :qrv"
+					});
+				});
+
+				it("Should send correct request on query.exec if querying main key with range key as less than comparison", async () => {
+					queryPromiseResolver = () => ({"Items": []});
+					Model = new dynamoose.Model("Cat", {"id": String, "name": String, "age": {"type": Number, "rangeKey": true}});
+					await callType.func(Model.query("id").eq("HelloWorld").where("age").lt(10).exec).bind(Model.query("id").eq("HelloWorld").where("age").lt(10))();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"ExpressionAttributeNames": {
+							"#qha": "id",
+							"#qra": "age"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "HelloWorld"},
+							":qrv": {"N": "10"}
+						},
+						"KeyConditionExpression": "#qha = :qhv AND #qra < :qrv"
+					});
+				});
+
+				it("Should send correct request on query.exec using range key as less than comparison", async () => {
+					queryPromiseResolver = () => ({"Items": []});
+					Model = new dynamoose.Model("Cat", {"id": String, "name": {"type": String, "index": {"global": true, "rangeKey": "age"}}, "age": Number});
+					await callType.func(Model.query("name").eq("Charlie").where("age").lt(10).exec).bind(Model.query("name").eq("Charlie").where("age").lt(10))();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"IndexName": "nameGlobalIndex",
+						"ExpressionAttributeNames": {
+							"#qha": "name",
+							"#qra": "age"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "Charlie"},
+							":qrv": {"N": "10"}
+						},
+						"KeyConditionExpression": "#qha = :qhv AND #qra < :qrv"
+					});
+				});
+
+				it.skip("Should send correct request on query.exec using range key as between comparison", async () => {
+					queryPromiseResolver = () => ({"Items": []});
+					Model = new dynamoose.Model("Cat", {"id": String, "name": {"type": String, "index": {"global": true, "rangeKey": "age"}}, "age": Number});
+					await callType.func(Model.query("name").eq("Charlie").where("age").between(10, 20).exec).bind(Model.query("name").eq("Charlie").where("age").between(10, 20))();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"IndexName": "nameGlobalIndex",
+						"ExpressionAttributeNames": {
+							"#qha": "name",
+							"#qra": "age"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "Charlie"},
+							":qrv-1": {"N": "10"},
+							":qrv-2": {"N": "20"}
+						},
+						"KeyConditionExpression": "#qha = :qhv AND #qra BETWEEN :qrv-1 AND :qrv-2"
+					});
+				});
+
+				it.skip("Should send correct request on query.exec using range key as exists comparison", async () => {
+					queryPromiseResolver = () => ({"Items": []});
+					Model = new dynamoose.Model("Cat", {"id": String, "name": {"type": String, "index": {"global": true, "rangeKey": "age"}}, "age": Number});
+					await callType.func(Model.query("name").eq("Charlie").where("age").exists().exec).bind(Model.query("name").eq("Charlie").where("age").exists())();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"IndexName": "nameGlobalIndex",
+						"ExpressionAttributeNames": {
+							"#qha": "name",
+							"#qra": "age"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "Charlie"}
+						},
+						"KeyConditionExpression": "#qha = :qhv AND #qra EXISTS"
+					});
+				});
+
+				it.skip("Should send correct request on query.exec using range key as contains comparison", async () => {
+					queryPromiseResolver = () => ({"Items": []});
+					Model = new dynamoose.Model("Cat", {"id": String, "name": {"type": String, "index": {"global": true, "rangeKey": "age"}}, "age": Number});
+					await callType.func(Model.query("name").eq("Charlie").where("age").contains(10).exec).bind(Model.query("name").eq("Charlie").where("age").contains(10))();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"IndexName": "nameGlobalIndex",
+						"ExpressionAttributeNames": {
+							"#qha": "name",
+							"#qra": "age"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "Charlie"},
+							":qrv": {"N": "10"}
+						},
+						"KeyConditionExpression": "#qha = :qhv AND contains(#qra, :qrv)"
+					});
+				});
+
+				it.skip("Should send correct request on query.exec using range key as beginsWith comparison", async () => {
+					queryPromiseResolver = () => ({"Items": []});
+					Model = new dynamoose.Model("Cat", {"id": String, "name": {"type": String, "index": {"global": true, "rangeKey": "age"}}, "age": Number});
+					await callType.func(Model.query("name").eq("Charlie").where("age").beginsWith(10).exec).bind(Model.query("name").eq("Charlie").where("age").beginsWith(10))();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"IndexName": "nameGlobalIndex",
+						"ExpressionAttributeNames": {
+							"#qha": "name",
+							"#qra": "age"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "Charlie"},
+							":qrv": {"N": "10"}
+						},
+						"KeyConditionExpression": "#qha = :qhv AND beginsWith(#qra, :qrv)"
+					});
+				});
+
 				it("Should send correct request on query.exec for one object passed in", async () => {
 					queryPromiseResolver = () => ({"Items": []});
 					await callType.func(Model.query({"name": "Charlie"}).exec).bind(Model.query({"name": "Charlie"}))();
