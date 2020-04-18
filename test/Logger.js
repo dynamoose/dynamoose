00b28d9260086c3aef5000c6fc7179ca57958e98
Charlie Fish <fishcharlie@me.com>
2020-04-18 17:30:58 -0600

    Adding emitter tests for logger

--- test/Logger.js
+++ test/Logger.js
@@ -180,4 +180,121 @@ describe("Logger", () => {
 			});
 		});
 	});
+
+	describe("Emitter", () => {
+		const emitter = require("../lib/logger/emitter");
+		let events = [];
+		class CustomProvider {
+			log(event) {
+				events.push(event);
+			}
+		}
+		beforeEach(() => {
+			dynamoose.logger.providers.set(new CustomProvider());
+		});
+		afterEach(() => {
+			events = [];
+		});
+
+		it("Should log event", () => {
+			emitter({"level": "info", "message": "Hello World", "category": "test"});
+			expect(events).to.be.an("array");
+			expect(events.length).to.eql(1);
+			expect(events[0]).to.be.an("object");
+			expect(Object.keys(events[0]).sort()).to.eql(["id", "timestamp", "level", "message", "category", "metadata"].sort());
+			expect(events[0].id).to.be.a("string");
+			expect(events[0].id).to.not.eql("");
+			expect(events[0].timestamp).to.be.within(new Date(Date.now() - 2000), new Date(Date.now() + 2000));
+			expect(events[0].level).to.eql("info");
+			expect(events[0].message).to.eql("Hello World");
+			expect(events[0].category).to.eql("test");
+			expect(events[0].metadata).to.eql({});
+		});
+
+		it("Should allow for no category", () => {
+			emitter({"level": "info", "message": "Hello World"});
+			expect(events[0].category).to.eql("");
+		});
+
+		it("Should not log event if paused", () => {
+			dynamoose.logger.pause();
+			emitter({"level": "info", "message": "Hello World", "category": "test"});
+			expect(events.length).to.eql(0);
+		});
+
+		it("Should throw error if no message passed in", () => {
+			expect(() => emitter({"level": "info"})).to.throw("You must pass in a valid message, level, and category into your event object.");
+		});
+
+		it("Should throw error if no level passed in", () => {
+			expect(() => emitter({"message": "Hello World"})).to.throw("You must pass in a valid message, level, and category into your event object.");
+		});
+
+		it("Should throw error if invalid level passed in", () => {
+			expect(() => emitter({"message": "Hello World", "level": "random"})).to.throw("You must pass in a valid message, level, and category into your event object.");
+		});
+
+		describe("Filter", () => {
+			describe("Level", () => {
+				const tests = [
+					{"filter": "error", "level": "error", "outcome": false},
+					{"filter": "error", "level": "fatal", "outcome": true},
+					{"filter": "error+", "level": "fatal", "outcome": false},
+					{"filter": "error+", "level": "error", "outcome": false},
+					{"filter": "error+", "level": "debug", "outcome": true},
+					{"filter": "error-", "level": "fatal", "outcome": true},
+					{"filter": "error-", "level": "error", "outcome": false},
+					{"filter": "error-", "level": "debug", "outcome": false},
+					{"filter": ["error", "info"], "level": "error", "outcome": false},
+					{"filter": ["error", "info"], "level": "info", "outcome": false},
+					{"filter": ["error", "info"], "level": "fatal", "outcome": true},
+				];
+				tests.forEach((test) => {
+					it(`Should ${test.outcome ? "" : "not "}filter level ${typeof test.filter === "object" ? JSON.stringify(test.filter) : `"${test.filter}"`} for input of ${test.level}`, () => {
+						dynamoose.logger.providers.set({
+							"provider": new CustomProvider(),
+							"filter": {
+								"level": test.filter
+							}
+						});
+						emitter({"level": test.level, "message": "Hello World"});
+						expect(events.length).to.eql(test.outcome ? 0 : 1);
+					});
+				});
+			});
+
+			describe("Category", () => {
+				const tests = [
+					{"filter": "aws:dynamodb:putItem:request", "category": "aws:dynamodb:putItem:request", "outcome": false},
+					{"filter": "aws:dynamodb:putItem:request", "category": "aws:dynamodb:putItem:response", "outcome": true},
+					{"filter": ["aws:dynamodb:putItem:request", "aws:dynamodb:putItem:response"], "category": "aws:dynamodb:putItem:response", "outcome": false},
+					{"filter": "*:dynamodb:putItem:request", "category": "aws:dynamodb:putItem:request", "outcome": false},
+					{"filter": "*:dynamodb:putItem:request", "category": "aws:dynamodb:putItem:request", "outcome": false},
+					{"filter": "**", "category": "aws:dynamodb:putItem:request", "outcome": false},
+					{"filter": "*:dynamodb:putItem:request", "category": "aws:dynamodb:getItem:request", "outcome": true},
+					{"filter": "*:dynamodb:**", "category": "aws:dynamodb:getItem:request", "outcome": false},
+					{"filter": "*:dynamodb:**", "category": "aws:dynamodb:getItem:response", "outcome": false},
+					{"filter": "*:dynamodb:**", "category": "aws:dynamodb:putItem:request", "outcome": false},
+					{"filter": "*:dynamodb:**", "category": "aws:dynamodb:putItem:response", "outcome": false},
+					{"filter": "random", "category": "other", "outcome": true},
+					{"filter": "random:*", "category": "other", "outcome": true},
+					{"filter": "random:*", "category": "random", "outcome": true},
+					{"filter": "random:**", "category": "random", "outcome": true},
+					{"filter": "random:*:test", "category": "random:test", "outcome": true},
+				];
+				tests.forEach((test) => {
+					it(`Should ${test.outcome ? "" : "not "}filter level ${typeof test.filter === "object" ? JSON.stringify(test.filter) : `"${test.filter}"`} for input of ${test.category}`, () => {
+						dynamoose.logger.providers.set({
+							"provider": new CustomProvider(),
+							"filter": {
+								"category": test.filter
+							}
+						});
+						emitter({"level": "error", "category": test.category, "message": "Hello World"});
+						expect(events.length).to.eql(test.outcome ? 0 : 1);
+					});
+				});
+			});
+		});
+	});
 });
