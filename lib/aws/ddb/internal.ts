e66be4f4c98aaa98cc4d51efafd71937c106530e
Charlie Fish <fishcharlie@me.com>
2020-04-18 22:27:23 -0600

    Converting more files to TypeScript

--- lib/aws/ddb/internal.ts
+++ lib/aws/ddb/internal.ts
@@ -1,7 +1,7 @@
-const ddb = require("./index");
-const log = require("../../logger/emitter");
+import ddb from "./index";
+import log from "../../logger/emitter";
 
-export = async (method, params) => {
+export = async (method: string, params) => {
 	log({"level": "debug", "category": `aws:dynamodb:${method}:request`, "message": JSON.stringify(params, null, 4), "payload": {"request": params}});
 	const result = await ddb()[method](params).promise();
 	log({"level": "debug", "category": `aws:dynamodb:${method}:response`, "message": typeof result === "undefined" ? "undefined" : JSON.stringify(result, null, 4), "payload": {"response": result}});
