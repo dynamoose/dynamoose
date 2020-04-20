172f6e26f15d2223c5e9cd802848d2003152b0f2
Charlie Fish <fishcharlie@me.com>
2020-04-20 00:53:52 -0600

    Improving documentation with getting started section

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -4,7 +4,7 @@ import {Redirect} from "@docusaurus/router";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
 function Home() {
-	return <Redirect to={useBaseUrl("/api/General")} />;
+	return <Redirect to={useBaseUrl("/getting_started/Introduction")} />;
 }
 
 export default Home;
