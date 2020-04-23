6960562fed9e93cd7f70af5420972ce7e3cfc7fb
Charlie Fish <fishcharlie@me.com>
2020-04-22 22:05:18 -0600

    Adding support for deploying to more sites

--- publish/information/retrieve.js
+++ publish/information/retrieve.js
@@ -1,11 +1,22 @@
 module.exports = (version) => {
 	const regex = /^v?((?:\d\.?){1,3})(?:-(.*)\.(\d*))?$/gmu;
 	const [,main,tag,tagNumber] = regex.exec(version);
-	return {
+	const obj = {
 		main,
 		tag,
 		"npmtag": tag || "latest",
 		tagNumber,
 		"isPrerelease": Boolean(tag)
 	};
+
+	switch(obj.npmtag) {
+	case "latest":
+		obj.websites3bucket = "dynamoosejs.com";
+		break;
+	default:
+		obj.websites3bucket = "";
+		break;
+	}
+
+	return obj;
 };
