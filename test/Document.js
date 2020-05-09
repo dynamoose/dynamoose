a40c002f24500fef8dab4696d2b025cc965f7d63
Charlie Fish <fishcharlie@me.com>
2020-05-08 22:03:01 -0600

    Adding tests for passing original object into set function

--- test/Document.js
+++ test/Document.js
@@ -1004,6 +1004,35 @@ describe("Document", () => {
 					}]);
 				});
 
+				it("Should pass in correct original variable into set function", async () => {
+					let newVal, oldVal;
+					aws.ddb.set({
+						"putItem": (params) => {
+							putParams.push(params);
+							return {"promise": putItemFunction};
+						},
+						"getItem": () => ({
+							"promise": () => Promise.resolve({"Item": {"id": {"N": "1"}, "name": {"S": "Charlie-set"}}})
+						})
+					});
+
+					putItemFunction = () => Promise.resolve();
+					User = dynamoose.model("User", {"id": Number, "name": {"type": String, "set": (newValA, oldValA) => {
+						newVal = newValA;
+						oldVal = oldValA;
+						return oldValA;
+					}}});
+					user = await User.get("1");
+					user.name = "Charlie";
+					await callType.func(user).bind(user)();
+					expect(newVal).to.eql("Charlie");
+					expect(oldVal).to.eql("Charlie-set");
+					expect(putParams).to.eql([{
+						"Item": {"id": {"N": "1"}, "name": {"S": "Charlie-set"}},
+						"TableName": "User"
+					}]);
+				});
+
 				it("Should save with correct object with async set function for attribute", async () => {
 					putItemFunction = () => Promise.resolve();
 					User = dynamoose.model("User", {"id": Number, "name": {"type": String, "set": async (val) => `${val}-set`}});
