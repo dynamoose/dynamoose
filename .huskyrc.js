7ebd5af596b752a9a3ab4edc511c9aa48774f9aa
Pavol Fulop <pavolfulop@gmail.com>
2020-05-17 11:19:27 +0200

    include eslint config, separate husky config

---
+++ .huskyrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  hooks: {
+    "pre-commit": "lint-staged",
+    "pre-push": "npm test",
+  },
+};
---
+++ lint-staged.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	"(lib|test|publish)/*.{js,ts}": ["npm run lint:fix"],
+};
