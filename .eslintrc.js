e14ba7f09eb0c575974f4d0cb717216983be48df
Pavol Fulop <pavolfulop@gmail.com>
2020-05-17 21:15:52 +0200

    explicit return type required

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
 		"plugin:@typescript-eslint/recommended",
 	],
 	"rules": {
-		"@typescript-eslint/explicit-function-return-type": "off",
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/no-unused-vars": "off",
 		"@typescript-eslint/camelcase": "off",
--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  hooks: {
-    "pre-commit": "lint-staged",
-    "pre-push": "npm test",
-  },
+	"hooks": {
+		"pre-commit": "lint-staged",
+		"pre-push": "npm test",
+	},
 };
