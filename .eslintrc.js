d629b477b9180f2788ba9144f0c6da465f1a62a1
Charlie Fish <fishcharlie@me.com>
2020-05-23 15:37:15 -0600

    Ignoring ESLint files in lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
 		"es6": true,
 		"node": true
 	},
-	"extends": "eslint:recommended",
+    "extends": "eslint:recommended",
 	"overrides": [
 		{
 			"files": ["**/*.ts"],
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,6 @@
+const { CLIEngine } = require('eslint');
+const cli = new CLIEngine({});
+
 module.exports = {
-	"*.{js,ts}": "npm run lint:fix --"
+	'*.{js,ts}': (files) => `npm run lint:fix -- ${files.filter((file) => !cli.isPathIgnored(file)).join(" ")}`
 };
