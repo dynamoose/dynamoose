e8ba679bf518b664002286f9e5f02d48ade44daf
Charlie Fish <fishcharlie@me.com>
2020-06-17 21:26:42 -0600

    Reorganzing test folder structure

---
+++ test/unit/utils/all_elements_match.js
@@ -0,0 +1,33 @@
+const {expect} = require("chai");
+const utils = require("../../../dist/utils");
+
+describe("utils.all_elements_match", () => {
+	it("Should be a function", () => {
+		expect(utils.all_elements_match).to.be.a("function");
+	});
+
+	const tests = [
+		{
+			"input": [],
+			"output": true
+		},
+		{
+			"input": ["hello"],
+			"output": true
+		},
+		{
+			"input": ["hello", "hello"],
+			"output": true
+		},
+		{
+			"input": ["hello", "world"],
+			"output": false
+		}
+	];
+
+	tests.forEach((test) => {
+		it(`Should return ${test.output} for ${test.input}`, () => {
+			expect(utils.all_elements_match(test.input)).to.eql(test.output);
+		});
+	});
+});
