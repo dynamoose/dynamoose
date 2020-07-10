39dff9c96000e687aa90990418f5539d219dd7d5
Rafal Wilinski <raf.wilinski@gmail.com>
2020-07-10 15:03:46 +0200

    chore: change spaces to tabs

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,46 +1,46 @@
 module.exports = {
-  sidebar: [
-    {
-      type: 'category',
-      label: 'Getting Started',
-      items: [
-        'getting_started/Introduction',
-        'getting_started/Install',
-        'getting_started/Import',
-        'getting_started/Configure',
-        'getting_started/TypeScript',
+	sidebar: [
+		{
+			type: 'category',
+			label: 'Getting Started',
+			items: [
+				'getting_started/Introduction',
+				'getting_started/Install',
+				'getting_started/Import',
+				'getting_started/Configure',
+				'getting_started/TypeScript',
       ],
-    },
-    {
-      type: 'category',
-      label: 'Guide',
-      items: [
-        'guide/Schema',
-        'guide/Model',
-        'guide/Document',
-        'guide/Condition',
-        'guide/Query',
-        'guide/Scan',
-        'guide/Transaction',
-        'guide/Dynamoose',
-      ],
-    },
-    {
+		},
+		{
+			type: 'category',
+			label: 'Guide',
+			items: [
+				'guide/Schema',
+				'guide/Model',
+				'guide/Document',
+				'guide/Condition',
+				'guide/Query',
+				'guide/Scan',
+				'guide/Transaction',
+				'guide/Dynamoose',
+			],
+		},
+		{
       type: 'category',
       label: 'Other',
       items: ['other/FAQ'],
-    },
-    {
-      type: 'category',
-      label: 'Sponsored By',
-      collapsed: false,
-      items: [
-        {
+		},
+		{
+			type: 'category',
+			label: 'Sponsored By',
+			collapsed: false,
+			items: [
+				{
           type: 'link',
           label: '',
           href: 'https://dynobase.dev/?ref=dynamoose',
-        },
-      ],
-    },
-  ],
+				},
+			],
+		},
+	],
 };
