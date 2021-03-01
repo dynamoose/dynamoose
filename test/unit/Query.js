aa17762094469c246b1fba93862ba6e3a3a3e786
Andrew Dassonville <dassonville.andrew@gmail.com>
2021-02-28 17:38:44 -0800

    Prioritize indexes with correct range key when querying

--- test/unit/Query.js
+++ test/unit/Query.js
@@ -293,6 +293,40 @@ describe("Query", () => {
 					});
 				});
 
+				it("Should send correct request on query.exec using array of indexes", async () => {
+					queryPromiseResolver = () => ({"Items": []});
+					Model = dynamoose.model("Cat", {"id": String, "name": {"type": String, "index": [{"global": true, "rangeKey": "age", "name": "AgeIndex"}, {"global": true, "rangeKey": "breed", "name": "BreedIndex"}]}, "age": Number, "breed": String});
+					await callType.func(Model.query("name").eq("Charlie").where("age").gt(10).exec).bind(Model.query("name").eq("Charlie").where("age").gt(10))();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"IndexName": "AgeIndex",
+						"ExpressionAttributeNames": {
+							"#qha": "name",
+							"#qra": "age"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "Charlie"},
+							":qrv": {"N": "10"}
+						},
+						"KeyConditionExpression": "#qha = :qhv AND #qra > :qrv"
+					});
+
+					await callType.func(Model.query("name").eq("Charlie").where("breed").eq("calico").exec).bind(Model.query("name").eq("Charlie").where("breed").eq("calico"))();
+					expect(queryParams).to.eql({
+						"TableName": "Cat",
+						"IndexName": "BreedIndex",
+						"ExpressionAttributeNames": {
+							"#qha": "name",
+							"#qra": "breed"
+						},
+						"ExpressionAttributeValues": {
+							":qhv": {"S": "Charlie"},
+							":qrv": {"S": "calico"}
+						},
+						"KeyConditionExpression": "#qha = :qhv AND #qra = :qrv"
+					});
+				});
+
 				it("Should send correct request on query.exec for one object passed in", async () => {
 					queryPromiseResolver = () => ({"Items": []});
 					await callType.func(Model.query({"name": "Charlie"}).exec).bind(Model.query({"name": "Charlie"}))();
