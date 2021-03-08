51a603731f0577883d1cfbae0d80e28dfbf1c991
Charlie Fish <fishcharlie@me.com>
2021-03-07 18:24:09 -0700

    Fixing bug with prefix and ModelStore

--- lib/ModelStore.ts
+++ lib/ModelStore.ts
@@ -2,14 +2,18 @@ import CustomError = require("./Error");
 import {Model} from "./Model";
 import {Document} from "./Document";
 
+let aliases: {[name: string]: string} = {};
 let models: {[name: string]: Model<Document>} = {};
 
 const returnObject = <T extends Document>(input: Model<T> | string): Model<T> | never => {
 	if (input instanceof Model) {
-		models[input.name] = input;
+		models[input.originalName] = input;
+		aliases[input.name] = input.originalName;
 		return input;
 	} else if (typeof input === "string") {
-		return models[input] as Model<T>;
+		const alias = aliases[input];
+		const result = models[input] || models[alias];
+		return result as Model<T>;
 	} else {
 		throw new CustomError.InvalidParameter("You must pass in a Model or table name as a string.");
 	}
