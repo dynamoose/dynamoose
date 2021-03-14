d3b24a9b5326c21957f922fc43d4186c39278b23
Charlie Fish <fishcharlie@me.com>
2021-03-13 17:43:46 -0700

    Using null name for type error instead of object

--- lib/utils/type_name.ts
+++ lib/utils/type_name.ts
@@ -2,5 +2,15 @@ import { DynamoDBSetTypeResult, DynamoDBTypeResult } from "../Schema";
 
 // This function takes in a value and returns a user string for the type of that value. This function is mostly used to display type errors to users.
 export default (value: any, typeDetailsArray: (DynamoDBTypeResult | DynamoDBSetTypeResult)[]): string => {
-	return `${typeof value}${typeDetailsArray.some((val) => val.name === "Constant") ? ` (${value})` : ""}`;
+	let str: string = "";
+	if (value === null) {
+		str += "null";
+	} else {
+		str += typeof value;
+	}
+
+	// Add constant value to type name
+	str += typeDetailsArray.some((val) => val.name === "Constant") ? ` (${value})` : "";
+
+	return str;
 };
