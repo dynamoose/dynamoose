5fa8cb0658eb9af48f82dd7e111382a0289517fd
Charlie Fish <fishcharlie@me.com>
2021-03-13 18:22:32 -0700

    Adding TypeScript test

--- test/types/model/Create.ts
+++ test/types/model/Create.ts
@@ -3,6 +3,7 @@
 import {UserTypedModel} from "../Model";
 
 const shouldCreateSuccessfully = UserTypedModel.create({"id": "1", "name": "john", "age": 25});
+const shouldAccessPropertySuccessfully = (await shouldCreateSuccessfully).id;
 
 const shouldNotFailWithNotAllAttributesPassedIn = UserTypedModel.create({"id": "1"});
 
