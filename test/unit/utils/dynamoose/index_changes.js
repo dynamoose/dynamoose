9bb720c86e42b444b0edb334fc25268407e82779
Charlie Fish <fishcharlie@me.com>
2021-06-11 19:42:51 -0600

    Removing table variable from test to pass lint

--- test/unit/utils/dynamoose/index_changes.js
+++ test/unit/utils/dynamoose/index_changes.js
@@ -195,7 +195,7 @@ describe("utils.dynamoose.index_changes", () => {
 	tests.forEach((test) => {
 		it(`Should return ${JSON.stringify(test.output)} for ${test.input}`, async () => {
 			const Model = dynamoose.model("Model", test.schema);
-			const Table = new dynamoose.Table("Table", [Model], {"create": false, "waitForActive": false, "update": false});
+			new dynamoose.Table("Table", [Model], {"create": false, "waitForActive": false, "update": false});
 			expect(await utils.dynamoose.index_changes(Model.Model, test.input)).to.eql(test.output);
 		});
 	});
