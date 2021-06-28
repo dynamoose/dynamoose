18ad3725da42149ab944b54aa98f25cc498d7c7a
Kishore Chavali <kishore.chavali@asurion.com>
2021-06-28 14:34:28 -0500

    Added Test Case for Buffer

--- test/unit/Schema.js
+++ test/unit/Schema.js
@@ -1228,7 +1228,11 @@ describe("Schema", () => {
 			{"schema": {"id": Number, "data": [{"type": Object, "schema": {"name": String, "id1": Number}}, {"type": Object, "schema": {"name": Number, "id2": String}}]}, "input": {"id": 1, "data": {"name": 1, "id2": 2}}, "output": {"data": 1}},
 			{"schema": {"id": Number, "data": String}, "input": {"id": 1, "data9": "Hello"}, "settings": {"includeAllProperties": true}, "output": {"id": {"index": 0, "entryCorrectness": [1], "matchCorrectness": 1}, "data9": {"index": 0, "entryCorrectness": [0.5], "matchCorrectness": 0.5}}},
 			{"schema": {"id": Number, "data": {"type": Object, "schema": {"parameter": String}}}, "input": {"id": 1, "data": {"parameter": "hello"}}, "settings": {"includeAllProperties": true}, "output": {"id": {"index": 0, "entryCorrectness": [1], "matchCorrectness": 1}, "data": {"index": 0, "entryCorrectness": [1], "matchCorrectness": 1}, "data.parameter": {"entryCorrectness": [1], "index": 0, "matchCorrectness": 1}}},
-			{"schema": {"id": Number, "data": {"type": Object, "schema": {"parameter": Number}}}, "input": {"id": 1, "data": {"parameter": "hello"}}, "settings": {"includeAllProperties": true}, "output": {"id": {"index": 0, "entryCorrectness": [1], "matchCorrectness": 1}, "data": {"index": 0, "entryCorrectness": [1], "matchCorrectness": 1}, "data.parameter": {"entryCorrectness": [0], "index": 0, "matchCorrectness": 0}}}
+			{"schema": {"id": Number, "data": {"type": Object, "schema": {"parameter": Number}}}, "input": {"id": 1, "data": {"parameter": "hello"}}, "settings": {"includeAllProperties": true}, "output": {"id": {"index": 0, "entryCorrectness": [1], "matchCorrectness": 1}, "data": {"index": 0, "entryCorrectness": [1], "matchCorrectness": 1}, "data.parameter": {"entryCorrectness": [0], "index": 0, "matchCorrectness": 0}}},
+			// this function is ideally used to determine exact type of the object when there is a choice in schema definition around a property , so this function should return {} for simple strict schemas  
+			// as demonstated in below two test cases , highlight simple strict schemas where this function just returns empty 
+			{"schema": {"id": String, "data": Buffer}, "input": {"id": "id1", "data": Buffer.from("hello world")}, "output": {}},
+			{"schema": {"id": String, "data": String}, "input": {"id": "id1", "data": "hello world"}, "output": {}},
 		];
 
 		tests.forEach((test) => {
