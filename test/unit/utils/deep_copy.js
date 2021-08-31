91b958047b01408e0bf1050b1216e1af36749f0c
Andrew Dassonville <dassonville.andrew@gmail.com>
2021-08-31 08:27:50 -0600

    Add additional deep_copy tests

--- test/unit/utils/deep_copy.js
+++ test/unit/utils/deep_copy.js
@@ -6,7 +6,57 @@ describe("utils.deep_copy", () => {
 		expect(utils.deep_copy).to.be.a("function");
 	});
 
-	it("Should return a copy of the object", () => {
+	it("Should return a deep copy of the passed string", () => {
+		let original = "Test";
+		const copy = utils.deep_copy(original);
+		expect(copy).to.equal("Test");
+
+		original = "Test 2";
+
+		expect(original).to.equal("Test 2");
+		expect(copy).to.equal("Test");
+	});
+
+	it("Should return a deep copy of the passed class", () => {
+		class Test {
+			constructor () {
+				this.test = "Test";
+			}
+		}
+
+		const original = new Test();
+		const copy = utils.deep_copy(original);
+		expect(copy.test).to.equal("Test");
+
+		original.test = "Test 2";
+
+		expect(original.test).to.equal("Test 2");
+		expect(copy.test).to.equal("Test");
+	});
+
+	it("Should return a deep copy of the passed date", () => {
+		const original = new Date(2021, 2, 1);
+		const copy = utils.deep_copy(original);
+		expect(copy.toUTCString()).to.equal("Mon, 01 Mar 2021 07:00:00 GMT");
+
+		original.setDate(2);
+
+		expect(original.toUTCString()).to.equal("Tue, 02 Mar 2021 07:00:00 GMT");
+		expect(copy.toUTCString()).to.equal("Mon, 01 Mar 2021 07:00:00 GMT");
+	});
+
+	it("Should return a deep copy of the passed array", () => {
+		const original = [{"a": 1}, {"a": 2}];
+		const copy = utils.deep_copy(original);
+		expect(copy).to.deep.equal([{"a": 1}, {"a": 2}]);
+
+		original[0].a = 2;
+
+		expect(original).to.deep.equal([{"a": 2}, {"a": 2}]);
+		expect(copy).to.deep.equal([{"a": 1}, {"a": 2}]);
+	});
+
+	it("Should return a deep copy of the passed object", () => {
 		const original = {"a": 1, "b": "test", "c": {"d": 100, "e": 200}};
 		const copy = utils.deep_copy(original);
 		expect(copy).to.deep.equal({"a": 1, "b": "test", "c": {"d": 100, "e": 200}});
