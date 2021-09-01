4f3f2eaaa98d5640ec45b5803c372a6b223f4d2b
Andrew Dassonville <dassonville.andrew@gmail.com>
2021-09-01 09:07:29 -0600

    Switch to lodash deepCopy

--- lib/utils/deep_copy.ts
+++ lib/utils/deep_copy.ts
@@ -1,29 +1,5 @@
+import * as cloneDeep from "lodash.clonedeep";
+
 export default function deep_copy<T> (obj: T): T {
-	let copy: any;
-
-	// Handle Date
-	if (obj instanceof Date) {
-		copy = new Date();
-		copy.setTime(obj.getTime());
-		return copy;
-	}
-
-	// Handle Array
-	if (obj instanceof Array) {
-		copy = obj.map((i) => deep_copy(i));
-		return copy;
-	}
-
-	// Handle Object
-	if (obj instanceof Object) {
-		copy = {};
-		for (const attr in obj) {
-			if (Object.prototype.hasOwnProperty.call(obj, attr)) {
-				copy[attr] = deep_copy(obj[attr]);
-			}
-		}
-		return copy;
-	}
-
-	return obj;
+	return cloneDeep(obj);
 }
