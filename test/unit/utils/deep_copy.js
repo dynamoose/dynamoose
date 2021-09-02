6d518081a16460507f07f02c7ed085da495c0d90
Charlie Fish <fishcharlie@me.com>
2021-09-02 15:58:38 -0600

    Using custom deep_copy method

--- test/unit/utils/deep_copy.js
+++ test/unit/utils/deep_copy.js
@@ -1,5 +1,6 @@
 const {expect} = require("chai");
 const utils = require("../../../dist/utils");
+const dynamoose = require("../../../dist");
 
 describe("utils.deep_copy", () => {
 	it("Should be a function", () => {
@@ -67,5 +68,99 @@ describe("utils.deep_copy", () => {
 
 		expect(original).to.deep.equal({"b": "test", "c": {"e": 200}});
 		expect(copy).to.deep.equal({"a": 1, "b": "test", "c": {"d": 100, "e": 200}});
+	});
+
+	it("Should return a deep copy of the passed class instances", () => {
+		class PersonWrapper {
+			constructor(name, age) {
+				this.name = name;
+				this.age = age;
+			}
+		}
+		const original = new PersonWrapper("Tim", 20);
+		const copy = utils.deep_copy(original);
+		expect(copy).to.deep.equal(new PersonWrapper("Tim", 20));
+
+		original.name = undefined;
+
+		expect(original).to.deep.equal(new PersonWrapper(undefined, 20));
+		expect(copy).to.deep.equal(new PersonWrapper("Tim", 20));
+		expect(original.constructor).to.deep.equal(PersonWrapper);
+		expect(copy.constructor).to.deep.equal(PersonWrapper);
+	});
+
+	it("Should return a deep copy of the passed multiple nested class instances", () => {
+		class NameWrapper {
+			constructor(name) {
+				this.name = name;
+			}
+		}
+
+		class PersonWrapper {
+			constructor(name, age) {
+				this.name = name;
+				this.age = age;
+			}
+		}
+		const original = new PersonWrapper(new NameWrapper("Tim"), 20);
+		const copy = utils.deep_copy(original);
+		expect(copy).to.deep.equal(new PersonWrapper(new NameWrapper("Tim"), 20));
+
+		original.name.name = undefined;
+		original.age = 25;
+
+		expect(original).to.deep.equal(new PersonWrapper(new NameWrapper(undefined), 25));
+		expect(copy).to.deep.equal(new PersonWrapper(new NameWrapper("Tim"), 20));
+		expect(original.constructor).to.deep.equal(PersonWrapper);
+		expect(copy.constructor).to.deep.equal(PersonWrapper);
+		expect(original.name.constructor).to.deep.equal(NameWrapper);
+		expect(copy.name.constructor).to.deep.equal(NameWrapper);
+	});
+
+	it("Should return a deep copy of the passed set", () => {
+		const original = new Set(["Hello", "World", "Universe"]);
+		const copy = utils.deep_copy(original);
+		expect([...copy]).to.deep.equal(["Hello", "World", "Universe"]);
+
+		original.delete("Hello");
+
+		expect([...original]).to.deep.equal(["World", "Universe"]);
+		expect([...copy]).to.deep.equal(["Hello", "World", "Universe"]);
+	});
+
+	it("Should return a deep copy of the passed DynamoDB set", () => {
+		const original = dynamoose.aws.converter().output({"SS": ["Hello", "World", "Universe"]});
+		const copy = utils.deep_copy(original);
+		expect(copy).to.deep.equal(dynamoose.aws.converter().output({"SS": ["Hello", "World", "Universe"]}));
+
+		original.values[0] = "Welcome";
+
+		expect(original).to.deep.equal(dynamoose.aws.converter().output({"SS": ["Welcome", "World", "Universe"]}));
+		expect(copy).to.deep.equal(dynamoose.aws.converter().output({"SS": ["Hello", "World", "Universe"]}));
+		expect(copy.constructor).to.deep.equal(original.constructor);
+	});
+
+	it("Should return a deep copy of the passed map", () => {
+		const original = new Map();
+		original.set("a", 1);
+		original.set("b", 2);
+		const copy = utils.deep_copy(original);
+		expect(Object.fromEntries(copy)).to.deep.equal({"a": 1, "b": 2});
+
+		original.delete("b");
+
+		expect(Object.fromEntries(original)).to.deep.equal({"a": 1});
+		expect(Object.fromEntries(copy)).to.deep.equal({"a": 1, "b": 2});
+	});
+
+	it("Should return a deep copy of the passed buffer", () => {
+		let original = Buffer.from("Hello World!");
+		const copy = utils.deep_copy(original);
+		expect(copy.toString()).to.deep.equal("Hello World!");
+
+		original[0] = 0;
+
+		expect(original.toString()).to.deep.equal("\u0000ello World!");
+		expect(copy.toString()).to.deep.equal("Hello World!");
 	});
 });
