095d994407d82d00589efb5b93166ec42d23a933
Charlie Fish <fishcharlie@me.com>
2021-11-20 20:36:37 -0700

    Removing chai-as-promised from timeout tests

--- __tests__/utils/timeout.js
+++ __tests__/utils/timeout.js
@@ -1,27 +1,24 @@
-const chaiAsPromised = require("chai-as-promised");
-const chai = require("chai");
-chai.use(chaiAsPromised);
-const {expect} = chai;
+const {expect: expectChai} = require("chai");
 const utils = require("../../dist/utils");
 
 describe("Timeout", () => {
 	it("Should be a function", () => {
-		expect(utils.timeout).to.be.a("function");
+		expectChai(utils.timeout).to.be.a("function");
 	});
 
 	it("Should return promise", () => {
 		const myTimeout = utils.timeout(1);
-		expect(myTimeout).to.be.a("promise");
+		expectChai(myTimeout).to.be.a("promise");
 	});
 
 	it("Should resolve in x milliseconds", async () => {
 		const ms = 10;
 		const timeA = Date.now();
 		await utils.timeout(ms);
-		expect(Date.now() - timeA).to.be.at.least(ms - 1); // Doing -1 here due to https://github.com/nodejs/node/issues/26578
+		expectChai(Date.now() - timeA).to.be.at.least(ms - 1); // Doing -1 here due to https://github.com/nodejs/node/issues/26578
 	});
 
 	it("Should reject if invalid number passed in", () => {
-		return expect(utils.timeout("test")).to.be.rejected;
+		return expect(utils.timeout("test")).rejects.toEqual("Invalid milliseconds passed in: test");
 	});
 });
