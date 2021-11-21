5a2c004df981b46af5a5daeef8b51c35a12a7af0
Charlie Fish <fishcharlie@me.com>
2021-11-21 09:42:35 -0700

    Adding childKey & parentKey helper methods

---
+++ __tests__/utils/parentKey.js
@@ -0,0 +1,12 @@
+const utils = require("../../dist/utils");
+
+describe("parentKey", () => {
+	const tests = [
+		["hello.world", "hello"],
+		["hello.world.universe", "hello.world"]
+	];
+
+	test.concurrent.each(tests)("parentKey(%s) = %s", (input, expected) => {
+		expect(utils.parentKey(input)).toEqual(expected);
+	});
+});
