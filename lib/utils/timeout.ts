95cfea0d11efce59df5ffb6630e70be503c9a546
Charlie Fish <fishcharlie@me.com>
2021-11-20 20:36:37 -0700

    Removing chai-as-promised from timeout tests

--- lib/utils/timeout.ts
+++ lib/utils/timeout.ts
@@ -2,7 +2,7 @@ export = (time: string | number): Promise<void> => {
 	const ms: number = typeof time === "string" ? parseInt(time) : time;
 	return new Promise((resolve, reject) => {
 		if (isNaN(ms)) {
-			reject(`Invalid miliseconds passed in: ${time}`);
+			reject(`Invalid milliseconds passed in: ${time}`);
 		}
 		setTimeout(() => resolve(), ms);
 	});
