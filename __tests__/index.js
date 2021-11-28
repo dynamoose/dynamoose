33556f5253d8896893582f92985c6749ab1c0102
Charlie Fish <fishcharlie@me.com>
2021-11-28 15:10:27 -0700

    Adding tests for new type helpers

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,8 +1,42 @@
-const {expect} = require("chai");
+const {expect: expectChai} = require("chai");
 const dynamoose = require("../dist");
 
 describe("dynamoose", () => {
 	it("Should return an object", () => {
-		expect(dynamoose).to.be.an("object");
+		expectChai(dynamoose).to.be.an("object");
+	});
+
+	describe("dynamoose.type", () => {
+		describe("dynamoose.type.CONSTANT", () => {
+			it("Should return correct object", () => {
+				expect(dynamoose.type.CONSTANT("Hello")).toEqual({
+					"value": "Constant",
+					"settings": {
+						"value": "Hello"
+					}
+				});
+			});
+		});
+
+		describe("dynamoose.type.COMBINE", () => {
+			it("Should return correct object if no separator defined", () => {
+				expect(dynamoose.type.COMBINE(["firstname", "lastname"])).toEqual({
+					"value": "Combine",
+					"settings": {
+						"attributes": ["firstname", "lastname"]
+					}
+				});
+			});
+
+			it("Should return correct object if separator defined", () => {
+				expect(dynamoose.type.COMBINE(["firstname", "lastname"], "-")).toEqual({
+					"value": "Combine",
+					"settings": {
+						"attributes": ["firstname", "lastname"],
+						"separator": "-"
+					}
+				});
+			});
+		});
 	});
 });
