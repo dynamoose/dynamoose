6bcb5f3ad73a6f96de0624c442e8e98f19619ee3
Charlie Fish <fishcharlie@me.com>
2021-12-15 17:43:53 -0700

    Removing old code

--- lib/utils/dynamoose/itemToJSON.ts
+++ lib/utils/dynamoose/itemToJSON.ts
@@ -1,6 +1,5 @@
 import {ItemArray, ObjectType} from "../../General";
 import {Item} from "../../Item";
-// import object from "../object";
 
 // TODO optimize this in the future after we add performance tests. Doing `JSON.parse(JSON.stringify()) can be kinda slow.
 export function itemToJSON (this: Item | ItemArray<Item>): ObjectType {
