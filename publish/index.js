fe3445c0f637eb22bc23f201b8890712e56febab
Charlie Fish <fishcharlie@me.com>
2022-01-13 19:34:22 -0700

    Fixing delete local branch for publish

--- publish/index.js
+++ publish/index.js
@@ -209,7 +209,9 @@ let package = require("../package.json");
 	await git.checkout(originalBranch);
 	gitCheckoutOriginal.succeed(`Checked out ${originalBranch} branch`);
 	const gitDeleteNewBranch = ora(`Deleting ${branch} branch`).start();
-	await git.deleteLocalBranch(branch, true);
+	// TODO: using `raw` instead of `deleteLocalBranch` until https://github.com/steveukx/git-js/issues/441 gets fixed
+	// await git.deleteLocalBranch(branch);
+	await git.raw(["branch", "-D", branch]);
 	gitDeleteNewBranch.succeed(`Deleted ${branch} branch`);
 	// Complete
 	process.exit(0);
