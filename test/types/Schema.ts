3bf16c50ec61d5579aa48f29a9734b9fff929a8a
karranb <karranrb@gmail.com>
2022-01-14 21:09:10 -0300

    test: check schema set method type for 2nd arg and async return

--- test/types/Schema.ts
+++ test/types/Schema.ts
@@ -97,3 +97,15 @@ const shouldSucceedWithArrayOfTypesInNestedSchema = new dynamoose.Schema({
 		]
 	}
 });
+const shouldSucceedWithAsyncSetMethodSchema = new dynamoose.Schema({
+	"id": {
+		"type": String,
+		"set": (value) => Promise.resolve(value)
+	}
+});
+const shouldSucceedWithSetMethodSecondArgSchema = new dynamoose.Schema({
+	"id": {
+		"type": String,
+		"set": (value, oldValue) => oldValue
+	}
+});
