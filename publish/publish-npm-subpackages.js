ef807c202e1149f3356ca1bb4e187640d3455be7
Charlie Fish <fishcharlie@me.com>
2022-02-12 11:50:59 -0700

    Adding more logs to publish subpackages

--- publish/publish-npm-subpackages.js
+++ publish/publish-npm-subpackages.js
@@ -4,14 +4,24 @@ const path = require("path");
 (async () => {
 	const workspacePackages = require("./workspacePackages");
 	for (const pkg of workspacePackages) {
+		console.log(`[${pkg}] Starting publish`);
+
+		console.log((await exec("which node")).output);
+		console.log((await exec("which npm")).output);
+		console.log((await exec("node -v")).output);
+		console.log((await exec("npm -v")).output);
+
+		console.log(`[${pkg}] npm install`);
 		await exec("npm install", {
 			"cwd": path.resolve(__dirname, "..", "workspaces", pkg)
 		});
+		console.log(`[${pkg}] npm publish --tag ${process.env.TAG}`);
 		await exec(`npm publish --tag ${process.env.TAG}`, {
 			"env": {
 				"NODE_AUTH_TOKEN": process.env.NODE_AUTH_TOKEN
 			},
 			"cwd": path.resolve(__dirname, "..", "workspaces", pkg)
 		});
+		console.log(`[${pkg}] Finished publish`);
 	}
 })();
