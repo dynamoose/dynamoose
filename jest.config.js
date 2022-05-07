7ef40049668161cec838d25d5f9166e84536f76e
Andrew Dassonville <dassonville.andrew@gmail.com>
2022-05-06 19:07:16 -0700

    Begin switching to Lerna

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	"setupFilesAfterEnv": ["./__tests__/_setup.js"],
-	"testPathIgnorePatterns": ["__tests__/_setup.js", "__tests__/.eslintrc.js"],
-	"coverageReporters": ["json", "lcov", "text", "html"],
-	"transformIgnorePatterns": ["dist/.+\\.js", "__tests__/.+\\.js"]
-};
