d45d0136b182132ed8bcc53d9b4a1a4c6b0d518e
Charlie Fish <fishcharlie@me.com>
2022-08-23 16:49:54 -0600

    Reverting change

--- packages/dynamoose/lib/ItemRetriever.ts
+++ packages/dynamoose/lib/ItemRetriever.ts
@@ -129,6 +129,7 @@ abstract class ItemRetriever extends InternalPropertiesClass<ItemRetrieverIntern
 
 	constructor (model: Model<Item>, typeInformation: ItemRetrieverTypeInformation, object?: ConditionInitializer) {
 		super();
+
 		let condition: Condition;
 		try {
 			condition = new Condition(object);
