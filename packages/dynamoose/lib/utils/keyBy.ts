a2c2e2ac8bceabdbee24dea8bc9719788374cc02
Andrey Madzhidov <famence@yandex.ru>
2022-11-03 21:28:08 +0600

    Rewrite the keyBy util

--- packages/dynamoose/lib/utils/keyBy.ts
+++ packages/dynamoose/lib/utils/keyBy.ts
@@ -1,5 +1,7 @@
 import {AnySimpleObject, AnySimpleValue} from "../Types";
 
-// Source: https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore
-export default <T = AnySimpleObject | AnySimpleValue>(array: T[], key: string) =>
-	(array || []).reduce((r, x) => ({...r, [key ? x[key] : x]: x}), {});
+export default <T = AnySimpleObject | AnySimpleValue>(array: T[] = [], key: string) =>
+	(array || []).reduce((result, item) => ({
+		...result,
+		[key ? item[key] : item]: item
+	}), {});
