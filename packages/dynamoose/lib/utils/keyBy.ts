9dd7bf2dff00285f5ab38ed6974ff0912174bb2e
Charlie Fish <fishcharlie@me.com>
2023-04-15 11:33:10 -0600

    Improving formatting of keyBy

--- packages/dynamoose/lib/utils/keyBy.ts
+++ packages/dynamoose/lib/utils/keyBy.ts
@@ -1,7 +1,12 @@
 import {AnySimpleObject, AnySimpleValue} from "../Types";
 
-export default <T = AnySimpleObject | AnySimpleValue>(array: T[] = [], key: string) =>
-	(array || []).reduce((result, item) => ({
+export default <T = AnySimpleObject | AnySimpleValue>(array: T[], key: string): AnySimpleObject => {
+	if (!array) {
+		return {};
+	}
+
+	return array.reduce((result, item) => ({
 		...result,
 		[key ? item[key] : item]: item
 	}), {});
+};
