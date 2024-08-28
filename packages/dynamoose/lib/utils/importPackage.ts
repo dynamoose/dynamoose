6fcd1f70c4c7d17b2673e42ac385462f698b48ae
Anay Paraswani <me@anayparaswani.dev>
2024-08-28 20:17:22 +0530

    Update importPackage.ts to Fix #1617

--- packages/dynamoose/lib/utils/importPackage.ts
+++ packages/dynamoose/lib/utils/importPackage.ts
@@ -4,7 +4,7 @@ export default async (name: string): Promise<any> => {
 	if (undefinedPackages.includes(name)) {
 		throw new Error("Package can not be found.");
 	} else {
-		const pkg = await import(name);
+		const pkg = await import(`${name}`);
 		return pkg;
 	}
 };
